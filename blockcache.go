913f28edcc4ce4f4a864e60f08a710e71dd44a3b
Ville Tuulos <ville.tuulos@adroll.com>
2016-11-18 18:31:55 -0800

    gofmt source code, cache handshake results

--- blockcache.go
+++ blockcache.go
@@ -1,187 +1,211 @@
 package main
 
 import (
-    "sync"
-    "time"
-    "fmt"
-    "errors"
+	"errors"
+	"fmt"
+	"sync"
+	"time"
 
-    "github.com/bluele/gcache"
-    "github.com/Workiva/go-datastructures/augmentedtree"
+	"github.com/Workiva/go-datastructures/augmentedtree"
+	"github.com/bluele/gcache"
 )
-/*
-# State
-
-- Interval Cache R
-- Expiration Cache E
-- Loader State L
-
-# Read Request
-- check interval cache R for an existing block
-    - found B
-        - get it from expiration cache E
-            - found:
-                - return the path
-            - not found:
-                - loader B
-    - not found
-        - define a new block B
-        - add B to R
-        - loader B
-
-# Loader, invoked if the key doesn't exist in E
-
-- incoming request for a block B
-- lock L
-- get B form L
-  - found:
-        - got WaitGroup G
-        - unlock L
-        - G.Wait()
-  - not found:
-        - put WaitGroup B->G in L
-        - G.Add(1)
-        - unlock L
-        - load B from S3
-        - put B in E
-        - lock L
-        - remove B->G from L
-        - unlock L
-        - G.done()
-- go to the begining of READ path
-
-#  Eviction function, invoked by E if E full
-
-- remove B from R
-- unlink B file
-- remove B from E
-
-*/
 
 type BlockResponse struct {
-    path string
-    offset uint64
-    size uint64
-}
-
-type RangeIndex struct {
-    index augmentedtree.Tree
-    lock sync.Mutex
+	path   string
+	offset uint64
+	size   uint64
 }
 
 type S3Loader func(string, string, string, uint64, uint64) (uint64, error)
 
 const MAX_RETRIES = 5
 
-var rangeIndicesLock sync.Mutex
-var rangeIndices map[string] RangeIndex
+/*
+This file takes care of three operations through three core data
+structures which are listed below:
 
+1. Map a new range request to previously cached ranges (blocks)
+   using an augmentedtree, RangeIndex. The ranges are specific to
+   each tdb so we need a separate mapping, rangeIndices, that maps
+   a tdb url to a RangeIndex.
+*/
+type RangeIndex struct {
+	index augmentedtree.Tree
+	lock  sync.Mutex
+}
+
+var rangeIndicesLock sync.Mutex
+var rangeIndices map[string]RangeIndex
+
+/*
+2. Keep track of usage of blocks so we can evict unused blocks.
+   This is accompished using an ARC cache blockCache that tracks
+   both recency and frequency of usage.
+*/
 var blockCache gcache.Cache
 
+/*
+3. When a block is missing from the cache, we need to fetch it
+   from S3. It is quite possible that multiple clients need
+   the same block, e.g. a new tdb, exactly at the same time but
+   it would be wasteful to download the same block from S3 multiple
+   times. We control this with loaderState that allows only one
+   loader to download a particular block while forcing others to
+   wait.
+*/
 var loaderStateLock sync.Mutex
-var loaderState map[string] *sync.WaitGroup
+var loaderState map[string]*sync.WaitGroup
 
 var blockSize uint64
 
+/*
+The block is missing. Fetch it from S3 while controlling the
+number of simultaneous loaders for this block.
+*/
 func loadBlock(reqBlock *ExtBlock, loader S3Loader) error {
-    key := reqBlock.GetKey()
-    loaderStateLock.Lock()
-    existingWG, ok := loaderState[key]
-    if ok {
-        loaderStateLock.Unlock()
-        existingWG.Wait()
-    } else {
-        var newWG sync.WaitGroup
-        newWG.Add(1)
-        loaderState[key] = &newWG
-        loaderStateLock.Unlock()
-        realSize, err := loader(reqBlock.GetDir(),
-                                reqBlock.GetKey(),
-                                reqBlock.GetPath(),
-                                reqBlock.GetOffset(),
-                                reqBlock.GetSize())
-        if err != nil {
-            fmt.Printf("loader failed: %s\n", err.Error())
-            return err
-        } else {
-            reqBlock.SetRealSize(realSize)
-            fmt.Printf("%s added to block cache\n", key)
-            blockCache.Set(key, reqBlock)
-        }
-        loaderStateLock.Lock()
-        delete(loaderState, key)
-        loaderStateLock.Unlock()
-        newWG.Done()
-    }
-    return nil
+	key := reqBlock.GetKey()
+	loaderStateLock.Lock()
+	existingWG, ok := loaderState[key]
+	if ok {
+		/* an existing loader active, wait */
+		loaderStateLock.Unlock()
+		existingWG.Wait()
+	} else {
+		/* this is the first loader, start loading, block others */
+		var newWG sync.WaitGroup
+		newWG.Add(1)
+		loaderState[key] = &newWG
+		loaderStateLock.Unlock()
+		realSize, err := loader(reqBlock.GetDir(),
+			reqBlock.GetKey(),
+			reqBlock.GetPath(),
+			reqBlock.GetOffset(),
+			reqBlock.GetSize())
+		if err != nil {
+			fmt.Printf("loader failed: %s\n", err.Error())
+			return err
+		} else {
+			/* block received successfully, add it to blockCache */
+			reqBlock.SetRealSize(realSize)
+			fmt.Printf("%s added to block cache\n", key)
+			blockCache.Set(key, reqBlock)
+		}
+		loaderStateLock.Lock()
+		delete(loaderState, key)
+		loaderStateLock.Unlock()
+		newWG.Done()
+	}
+	return nil
 }
 
+/*
+main function to request block from the cache or S3
+*/
 func getBlockFromCache(url string,
-                       offset uint64,
-                       minSize uint64,
-                       loader S3Loader) (BlockResponse, error) {
+	offset uint64,
+	minSize uint64,
+	loader S3Loader) (BlockResponse, error) {
 
-    reqSize := blockSize
-    if minSize > blockSize {
-        reqSize = minSize
-    }
-    reqBlock := NewExtBlock(url, offset, reqSize)
+	/*
+	   create a new ExtBlock for this request. This may get replaced
+	   by another cached block if we find a match..
+	*/
+	reqSize := blockSize
+	if minSize > blockSize {
+		reqSize = minSize
+	}
+	reqBlock := NewExtBlock(url, offset, reqSize)
 
-    for i := 0; i < MAX_RETRIES; i++ {
+	for i := 0; i < MAX_RETRIES; i++ {
 
-        rangeIndicesLock.Lock()
-        rangeIndex, ok := rangeIndices[url]
-        if !ok {
-            rangeIndex = RangeIndex{index: augmentedtree.New(1)}
-            rangeIndices[url] = rangeIndex
-        }
-        rangeIndex.lock.Lock()
-        rangeIndicesLock.Unlock()
+		/*
+		   find existing blocks in the range cache for this url which may
+		   include this block as a subrange
+		*/
+		rangeIndicesLock.Lock()
+		rangeIndex, ok := rangeIndices[url]
+		if !ok {
+			rangeIndex = RangeIndex{index: augmentedtree.New(1)}
+			rangeIndices[url] = rangeIndex
+		}
+		rangeIndex.lock.Lock()
+		rangeIndicesLock.Unlock()
 
-        var match augmentedtree.Interval = nil
-        for _, block := range rangeIndex.index.Query(reqBlock) {
-            if offset >= uint64(block.LowAtDimension(1)) &&
-               uint64(block.HighAtDimension(1)) - offset >= minSize - 1 {
-                match = block
-                break
-            }
-        }
-        if match == nil {
-            fmt.Printf("Adding a new entry in range cache for %s offset %d\n", url, offset)
-            rangeIndex.index.Add(reqBlock)
-            rangeIndex.lock.Unlock()
-            match = reqBlock
-        }else{
-            rangeIndex.lock.Unlock()
-            key := (match.(*ExtBlock)).GetKey()
-            fmt.Printf("Found a match for %s (%d) -> %s (%d)\n", reqBlock.GetKey(), offset, key, (match.(*ExtBlock)).GetOffset())
-            val, err := blockCache.GetIFPresent(key)
-            if err == nil {
-                res := val.(*ExtBlock)
-                path := fmt.Sprintf("%s/%s", res.GetDir(), res.GetKey())
-                respOffset := offset - uint64(res.LowAtDimension(1))
-                respSize := res.GetRealSize() - respOffset
-                return BlockResponse{path: path,
-                                     offset: respOffset,
-                                     size: respSize}, nil
-            }
-        }
-        err := loadBlock(match.(*ExtBlock), loader)
-        if err != nil {
-            fmt.Printf("[warn] Load block failed: %s", err.Error())
-            time.Sleep(10 * time.Second)
-        }
-    }
-    return BlockResponse{},
-           errors.New(fmt.Sprintf("GetBlock failed for %s (offset %d size %d)",
-                                  url, offset, minSize))
+		var match augmentedtree.Interval = nil
+		for _, block := range rangeIndex.index.Query(reqBlock) {
+			/*
+			   a matching block must include the full range of the requested
+			   block
+			*/
+			if offset >= uint64(block.LowAtDimension(1)) &&
+				uint64(block.HighAtDimension(1))-offset >= minSize-1 {
+				match = block
+				break
+			}
+		}
+		if match == nil {
+			/* no matching block found in the cache, use the loader */
+			fmt.Printf("Adding a new entry in range cache for %s offset %d\n", url, offset)
+			rangeIndex.index.Add(reqBlock)
+			rangeIndex.lock.Unlock()
+			match = reqBlock
+		} else {
+			/*
+			   a matching range found in the range cache. Next we need to check
+			   that it actually exists in blockCache (it might not exists, e.g. due
+			   to being loaded right now.
+			*/
+			rangeIndex.lock.Unlock()
+			key := (match.(*ExtBlock)).GetKey()
+			fmt.Printf("Found a match for %s (%d) -> %s (%d)\n", reqBlock.GetKey(), offset, key, (match.(*ExtBlock)).GetOffset())
+			val, err := blockCache.GetIFPresent(key)
+			if err == nil {
+				fmt.Printf("Cache hit for %s\n", key)
+				/* the block found in the cache, return the result */
+				res := val.(*ExtBlock)
+				path := fmt.Sprintf("%s/%s", res.GetDir(), res.GetKey())
+				respOffset := offset - uint64(res.LowAtDimension(1))
+				respSize := res.GetRealSize() - respOffset
+				return BlockResponse{path: path,
+					offset: respOffset,
+					size:   respSize}, nil
+			}
+		}
+		/* no previously cached block found, use the loader */
+		err := loadBlock(match.(*ExtBlock), loader)
+		if err != nil {
+			s3err, ok := err.(*s3error)
+			if ok && s3err.notFound {
+				return BlockResponse{}, err
+			} else {
+				/*
+				   loader failed, hopefully a temporary issue, let's try again
+				*/
+				fmt.Printf("[warn] Load block failed: %s", err.Error())
+				time.Sleep(10 * time.Second)
+			}
+		}
+	}
+	/*
+	   all attempts failed to download the block.
+	   The client may try again later */
+	return BlockResponse{},
+		errors.New(fmt.Sprintf("GetBlock failed for %s (offset %d size %d)",
+			url, offset, minSize))
 }
 
+func evict(keyIn interface{}, valueIn interface{}) {
+	/* TODO delete the file actually */
+	key := keyIn.(string)
+	fmt.Printf("Evict %s\n", key)
+}
+
+/* initialize the three caches */
 func initCache(maxSize uint64, blockSize0 uint64) {
-    blockSize = blockSize0
-    rangeIndices = make(map[string] RangeIndex)
-    loaderState = make(map[string] *sync.WaitGroup)
-    numBlocks := int((maxSize * 1024 * 1024) / BLOCKSIZE)
-    /* NOTE add evictionfunction */
-    blockCache = gcache.New(numBlocks).ARC().Build()
+	blockSize = blockSize0
+	rangeIndices = make(map[string]RangeIndex)
+	loaderState = make(map[string]*sync.WaitGroup)
+	numBlocks := int((maxSize * 1024 * 1024) / BLOCKSIZE)
+	/* TODO pre-populate cache with entries from disk */
+	blockCache = gcache.New(numBlocks).ARC().EvictedFunc(evict).Build()
 }
--- extblock.go
+++ extblock.go
@@ -1,104 +1,107 @@
 package main
 
 import (
-    "fmt"
-    "strconv"
-    "strings"
-    "crypto/md5"
-    "encoding/base64"
-    "encoding/binary"
-    "encoding/hex"
+	"crypto/md5"
+	"encoding/base64"
+	"encoding/binary"
+	"encoding/hex"
+	"fmt"
+	"strconv"
+	"strings"
 
-    "github.com/Workiva/go-datastructures/augmentedtree"
+	"github.com/Workiva/go-datastructures/augmentedtree"
 )
 
+/*
+ExtBlock is a simple container struct that implements the
+augmentedtree.Interval interface, so it can be stored in the
+range cache. It also defines how to map (url, offset, size)
+ranges to local filenames.
+*/
 type ExtBlock struct {
-    offset      uint64
-    blockSize   uint64
-    realSize    uint64
-    key         string
-    dir         string
-    path        string
-    hash        [16]byte
+	offset    uint64
+	blockSize uint64
+	realSize  uint64
+	key       string
+	dir       string
+	path      string
+	hash      [16]byte
 }
 
 func (e ExtBlock) LowAtDimension(dim uint64) int64 {
-    return int64(e.offset)
+	return int64(e.offset)
 }
 
 func (e ExtBlock) HighAtDimension(dim uint64) int64 {
-    return int64(e.offset + e.blockSize - 1)
+	return int64(e.offset + e.blockSize - 1)
 }
 
 func (e ExtBlock) OverlapsAtDimension(other augmentedtree.Interval, dim uint64) bool {
-    return (other.LowAtDimension(dim) >= e.LowAtDimension(dim) &&
-            other.LowAtDimension(dim) <= e.HighAtDimension(dim))
+	return (other.LowAtDimension(dim) >= e.LowAtDimension(dim) &&
+		other.LowAtDimension(dim) <= e.HighAtDimension(dim))
 }
 
 func (e ExtBlock) ID() uint64 {
-    return binary.LittleEndian.Uint64(e.hash[:8])
+	return binary.LittleEndian.Uint64(e.hash[:8])
 }
 
 func (e *ExtBlock) GetKey() string {
-    return e.key
+	//return e.key
+	return hex.EncodeToString(e.hash[:])
 }
 
 func (e *ExtBlock) GetSize() uint64 {
-    return e.blockSize
+	return e.blockSize
 }
 
 func (e *ExtBlock) GetPath() string {
-    return e.path
+	return e.path
 }
 
 func (e *ExtBlock) GetOffset() uint64 {
-    return e.offset
+	return e.offset
 }
 
 func (e *ExtBlock) GetDir() string {
-    return hex.EncodeToString(e.hash[:1])
+	return hex.EncodeToString(e.hash[:1])
 }
 
 func (e *ExtBlock) SetRealSize(size uint64) {
-    e.realSize = size
+	e.realSize = size
 }
 
 func (e *ExtBlock) GetRealSize() uint64 {
-    return e.realSize
+	return e.realSize
 }
 
 func NewExtBlock(path string, offset uint64, size uint64) *ExtBlock {
-    keyStr := fmt.Sprintf("%s %d %d", path, offset, size)
-    key := base64.URLEncoding.EncodeToString([]byte(keyStr))
-    return &ExtBlock{key: key,
-                     offset: offset,
-                     blockSize: size,
-                     path: path,
-                     hash: md5.Sum([]byte(key))}
+	keyStr := fmt.Sprintf("%s %d %d", path, offset, size)
+	key := base64.URLEncoding.EncodeToString([]byte(keyStr))
+	return &ExtBlock{key: key,
+		offset:    offset,
+		blockSize: size,
+		path:      path,
+		hash:      md5.Sum([]byte(key))}
 }
 
 func ParseExtBlock(key string, realSize uint64) (*ExtBlock, error) {
-    decoded, err := base64.URLEncoding.DecodeString(key)
-    if err != nil {
-        return nil, err
-    }
-    parts := strings.Split(string(decoded), " ")
-    offset, err := strconv.ParseUint(parts[1], 10, 64)
-    if err != nil {
-        return nil, err
-    }
-    blockSize, err := strconv.ParseUint(parts[2], 10, 64)
-    if err != nil {
-        return nil, err
-    }
-    return &ExtBlock{key: key,
-                     offset: offset,
-                     blockSize: blockSize,
-                     realSize: realSize,
-                     path: parts[0],
-                     hash: md5.Sum([]byte(key))}, nil
+	decoded, err := base64.URLEncoding.DecodeString(key)
+	if err != nil {
+		return nil, err
+	}
+	parts := strings.Split(string(decoded), " ")
+	offset, err := strconv.ParseUint(parts[1], 10, 64)
+	if err != nil {
+		return nil, err
+	}
+	blockSize, err := strconv.ParseUint(parts[2], 10, 64)
+	if err != nil {
+		return nil, err
+	}
+	return &ExtBlock{key: key,
+		offset:    offset,
+		blockSize: blockSize,
+		realSize:  realSize,
+		path:      parts[0],
+		hash:      md5.Sum([]byte(key))}, nil
 }
-
-
-
-
